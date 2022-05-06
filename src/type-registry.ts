ec4de14979b816acf9d4bb7753754db179e7c938
Isaac Valdez <isaacv123456789@gmail.com>
2022-05-06 17:28:23 -0500

    changes to type registry behavoir when there is literal 'object' type registered

--- src/type-registry.ts
+++ src/type-registry.ts
@@ -1,3 +1,5 @@
+import { TypeofType } from "./typeof-type"
+
 export class TypeRegistry<T> {
     private _map = new Map<Function, T>()
     /**
@@ -5,19 +7,47 @@ export class TypeRegistry<T> {
      */
     private _searchTypes: Function[] = []
 
-    private _literalMap = new Map<string, T>()
+    private _literalMap = new Map<TypeofType, T>()
 
-    register(type: string | Function, value: T): void {
+    types() {
+        return [
+                ...this._literalMap.keys(),
+                ...this._map.keys()
+            ]
+    }
+
+    values() {
+        return [
+                ...this._literalMap.values(),
+                ...this._map.values()
+            ]
+    }
+
+    entries(): [TypeofType | Function, T][] {
+        return [
+                ...this._literalMap.entries(),
+                ...this._map.entries()
+            ]
+    }
+
+    register(type: TypeofType | Function, value: T): void {
         if(typeof type == 'string') {
-            this.registerLiteral(type, value)
+            this.registerLiteralType(type, value)
         }
         else {
             this.registerType(type, value)
         }
     }
 
-    registerLiteral(type: string, value: T): void {
+    registerLiteralType(type: TypeofType, value: T): void {
         this._literalMap.set(type, value)
+    }
+
+    unregisterLiteralType(type: TypeofType): void {
+        const unregistered = this._literalMap.delete(type)
+        if(!unregistered) {
+            throw new Error(`Literal type "${type}" was not registered.`)
+        }
     }
 
     registerType<Type extends Function>(type: Type, value: T): void {
@@ -38,20 +68,41 @@ export class TypeRegistry<T> {
         this._searchTypes.splice(insertIndex, 0, type)
     }
 
+
+    unregisterType<Type extends Function>(type: Type): void {
+        const searchIndex = this._searchTypes.indexOf(type)
+        const removed = this._map.delete(type)
+        if(searchIndex < 0 || !removed) {
+            throw new Error(`Type "${type}" was not registered.`)
+        }
+
+        this._searchTypes.splice(searchIndex, 1)
+    }
+
     get(obj: any): T {
-        const literalValue = this._literalMap.get(typeof obj)
-        if(literalValue !== undefined) {
-            return literalValue
-        }
-        
-        for(let i = this._searchTypes.length - 1; i >= 0; i--) {
-            let type = this._searchTypes[i]
-
-            if(obj instanceof type) {
-                return this._map.get(type)!
+        if(typeof obj !== 'object') {
+            const literalValue = this._literalMap.get(typeof obj)
+            if(literalValue !== undefined) {
+                return literalValue
             }
-        }
 
-        throw new Error("Type / suitable supertype not registered")
+            throw new Error(`Entry for literal type "${typeof obj}" not registered.`)
+        }
+        else {
+            for(let i = this._searchTypes.length - 1; i >= 0; i--) {
+                let type = this._searchTypes[i]
+
+                if(obj instanceof type) {
+                    return this._map.get(type)!
+                }
+            }
+
+            const literalValue = this._literalMap.get(typeof obj)
+            if(literalValue !== undefined) {
+                return literalValue
+            }
+            
+            throw new Error(`Entry for type or supertype of ${obj} not registered.`)
+        }
     }
 }
--- test/type-registry.ts
+++ test/type-registry.ts
@@ -1,6 +1,6 @@
 import { suite, test } from '@testdeck/mocha'
 import { assert } from 'chai'
-import { TypeRegistry } from '../src'
+import { TypeofType, TypeRegistry } from '../src'
 
 @suite
 class TypeRegistryTests {
@@ -8,9 +8,14 @@ class TypeRegistryTests {
     insertAndFind_literal_1() {
         this.insertAndFind(
                 [
-                    [typeof true, 'this is for booleans'],
-                    [typeof 123, 'this is for numbers'],
-                    [typeof 'abc', 'this is for strings'],
+                    ['boolean', 'this is for booleans'],
+                    ['number', 'this is for numbers'],
+                    ['string', 'this is for strings'],
+                    ['bigint', 'this is for bigints'],
+                    ['symbol', 'this is for symbols'],
+                    ['undefined', 'this is for undefined'],
+                    ['object', 'this is for objects'],
+                    ['function', 'this is for functions'],
                 ],
                 ['hello', 'this is for strings'],
                 [534, 'this is for numbers'],
@@ -19,6 +24,14 @@ class TypeRegistryTests {
                 [true, 'this is for booleans'],
                 [false, 'this is for booleans'],
                 ['goodbye', 'this is for strings'],
+                [BigInt(23), 'this is for bigints'],
+                [BigInt(-100), 'this is for bigints'],
+                [Symbol(), 'this is for symbols'],
+                [Symbol.toStringTag, 'this is for symbols'],
+                [undefined, 'this is for undefined'],
+                [console, 'this is for objects'],
+                [{ a: 10, b: 20 }, 'this is for objects'],
+                [this.insertAndFind, 'this is for functions']
             )
     }
 
@@ -45,7 +58,7 @@ class TypeRegistryTests {
 
         this.insertAndFind(
                 [
-                    [typeof 'strngs', cages.other],
+                    ['string', cages.other],
                     [Fish, cages.pond],
                     [Amphibian, cages.pond],
                     [Frog, cages.plains],
@@ -67,8 +80,45 @@ class TypeRegistryTests {
             )
     }
 
+    @test
+    insertAndFind_classes_and_literal_1() {
+        class A{}
+        class B extends A {}
+        class C extends A {}
+        class D {}
+
+        const mappings = {
+            A: 'A maps here',
+            B: 'B maps here',
+            otherObjects: 'other objects map here',
+            functions: 'all functions map here',
+            undefined: 'undefined maps here'
+        }
+
+        this.insertAndFind(
+                [
+                    [A, mappings.A],
+                    [B, mappings.B],
+                    ['object', mappings.otherObjects],
+                    ['function', mappings.functions],
+                    ['undefined', mappings.undefined],
+                ],
+                [new A(), mappings.A],
+                [new B(), mappings.B],
+                [new C(), mappings.A],
+                [new D(), mappings.otherObjects],
+                [console, mappings.otherObjects],
+                [() => 123, mappings.functions],
+                [A, mappings.functions],
+                [B, mappings.functions],
+                [C, mappings.functions],
+                [D, mappings.functions],
+                [undefined, mappings.undefined],
+            )
+    }
+
     insertAndFind<T>(
-            toRegister: [Function | string, T][],
+            toRegister: [Function | TypeofType, T][],
             ...toFind: [any, T][]
         ): void {
         let registry = new TypeRegistry()
